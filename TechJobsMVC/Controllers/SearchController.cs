2a1112b132b07f77bd656ad8f904f83d21e3dd24
SusanPasek <susanpasekm@gmail.com>
2022-01-10 09:18:15 -0600

    completed A3

--- TechJobsMVC/Controllers/SearchController.cs
+++ TechJobsMVC/Controllers/SearchController.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Mvc;
 using TechJobsMVC.Models;
 using TechJobsMVC.Data;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace TechJobsMVC.Controllers
 {
